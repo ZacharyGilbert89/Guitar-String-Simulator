06144ea3baf9e0204998579af93353fb72a7a43b
Zach_Gilbert <99516776+ZacharyGilbert89@users.noreply.github.com>
2023-10-06 16:41:29 -0400

    Update KSGuitarSim.cpp

--- KSGuitarSim.cpp
+++ KSGuitarSim.cpp
@@ -11,38 +11,39 @@
 #include <SFML/System.hpp>
 #include <SFML/Audio.hpp>
 #include <SFML/Window.hpp>
-#define CONCERT_A 440.0
+#define CONCERT_A 440.0    
 #define SAMPLES_PER_SEC 44100
+//Global variables for the starting note, and how many samples per second for the quality of the sound
 std::vector<sf::Int16> makeSamples(StringSound& gs) { // NOLINT
     std::vector<sf::Int16> samples;
-    gs.pluck();
+    gs.pluck(); //creates a pluck call
     int duration = 8;  // seconds
-    int i;
+    int i; //temp variable for the loop
     for (i= 0; i < SAMPLES_PER_SEC * duration; i++) {
         gs.tic();
         samples.push_back(gs.sample());
     }
-    return samples;
+    return samples; //we return the samples
 }
 int main() {
     sf::RenderWindow window(sf::VideoMode(300, 300),
-    "SFML Plucked String Sound");
+    "SFML Plucked String Sound"); //creates the piano roll
     sf::Event event;
-    double freq;
-    std::vector<sf::Int16> samples;
-    std::vector<std::vector<int16_t>> sample(37);
-    std::vector<sf::Sound> sounds(37);
-    std::vector<sf::SoundBuffer> soundBuffers(37);
-    std::string keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
+    double freq; //keeps track of the frequency of the note
+    std::vector<sf::Int16> samples; //we create this to keep track of the samples
+    std::vector<std::vector<int16_t>> sample(37); // we want a samples size of 37
+    std::vector<sf::Sound> sounds(37); // we want a sound size of 37
+    std::vector<sf::SoundBuffer> soundBuffers(37); // we want a buffer size of 37
+    std::string keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' "; //used to keep track of the keyboard keys to assign a note per key press
 for (int i = 0; i < 37; i++) {
-        freq = CONCERT_A * pow(2, ((i - 24)/12.0));
-        StringSound gs1(freq);
-        sample[i] = makeSamples(gs1);
-        if (!soundBuffers[i].loadFromSamples(&sample[i][0],
+        freq = CONCERT_A * pow(2, ((i - 24)/12.0)); //We create a starting frequency based off the starting note, and then we pitch it up or down depending on the key that was pressed
+        StringSound gs1(freq); //Here we create the object that will hold all information about the string note, and the degredation of the note
+        sample[i] = makeSamples(gs1); //creates the samples so we can audibly recreate them
+        if (!soundBuffers[i].loadFromSamples(&sample[i][0], //Here we load the samples into the buffer so it can be played
         sample[i].size(), 2, SAMPLES_PER_SEC))
         throw std::runtime_error
         ("sf::SoundBuffer: failed to load from samples.");
-    sounds[i].setBuffer(soundBuffers[i]);
+    sounds[i].setBuffer(soundBuffers[i]); //Here we load the sound based off the buffer of the sound
     }
     while (window.isOpen()) {
         while (window.pollEvent(event)) {
@@ -51,19 +52,19 @@ for (int i = 0; i < 37; i++) {
                     window.close();
                     break;
                 case sf::Event::TextEntered:
-                    if (event.text.unicode) {
+                    if (event.text.unicode) { //This is for receiving the key that was pressed
                     int key;
                     key = keyboard.find(event.text.unicode);
                     if (key != -1) {
-                        sounds[key].play();
+                        sounds[key].play(); //play the sound
                     }
                 }
                 default:
                 break;
             }
-            window.clear();
-            window.display();
+            window.clear(); //clear window
+            window.display(); //clear the display of any artifacts left behind
         }
     }
-    return 0;
+    return 0; //exit
 }
