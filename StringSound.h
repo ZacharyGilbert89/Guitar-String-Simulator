2f9b3704d43205e0fb61b12c0bf82ad519cc4c25
Zach_Gilbert <99516776+ZacharyGilbert89@users.noreply.github.com>
2023-10-06 16:50:06 -0400

    Update StringSound.h

--- StringSound.h
+++ StringSound.h
@@ -4,19 +4,19 @@
 #include "CircularBuffer.h"
 #include <vector>
 #include <SFML/System.hpp>
-class StringSound {
+class StringSound { //StringSound Class
  public:
-    explicit StringSound(double frequency);
-    explicit StringSound(std::vector<sf::Int16> init);
+    explicit StringSound(double frequency); //constructor
+    explicit StringSound(std::vector<sf::Int16> init);//constructor
     StringSound (const StringSound &obj) = delete;
-    ~StringSound();
-    void pluck();
-    void tic();
-    sf::Int16 sample();
-    int time();
+    ~StringSound(); //deconstructor
+    void pluck(); //function for the pluck simulation
+    void tic(); //function for the degredation of the note
+    sf::Int16 sample(); //variable to hold the samples
+    int time(); //time of the note
  private:
-    CircularBuffer * _cb;
-    int _time;
+    CircularBuffer * _cb; //Circuler buffer
+    int _time; //time of the sound
 };
 
 #endif //NOLINT
