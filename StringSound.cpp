8052dfdeeaa72b278d313f4360c839e57631c63d
Zach_Gilbert <99516776+ZacharyGilbert89@users.noreply.github.com>
2023-10-06 16:48:05 -0400

    Update StringSound.cpp

--- StringSound.cpp
+++ StringSound.cpp
@@ -3,7 +3,7 @@
 #include <math.h>
 #include <random>
 StringSound::StringSound(double frequency) {
-    _cb = new CircularBuffer(ceil(44100/frequency));
+    _cb = new CircularBuffer(ceil(44100/frequency)); //creates a buffer based off the inputted frequency
 }
 StringSound::StringSound(std::vector<sf::Int16> init) {
     _cb = new CircularBuffer(init.size());
@@ -15,26 +15,26 @@ void StringSound::pluck() {
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> distr(-32768, 32767);
-    if (_cb->isEmpty() == true) {
+    if (_cb->isEmpty() == true) { //checks to see if the buffer has been cleared yet, if its clear, then we update the buffer
         for (int i = 0; i < _cb->getCapacity(); i++) {
             _cb->enqueue(distr(gen));
         }
     } else {
         for (int i = 0; i < _cb->getCapacity(); i++) {
-            _cb->dequeue();
+            _cb->dequeue(); //starts the dequeue process if the buffer is not empty
         }
     }
 }
-void StringSound::tic() {
-    double newVibration;
+void StringSound::tic() { //this function creates the degredation of the frequency of the note, similar to a guitar string slowly decreasing its frequency
+    double newVibration; //variable to hold teh degredation amount
     newVibration = .996 * (.5 * (_cb->dequeue() + _cb->peek()));
-    _cb->enqueue(newVibration);
-    _time++;
+    _cb->enqueue(newVibration); //here we load the buffer with new degraded samples
+    _time++; //Duration of the note
 }
 int StringSound::time() {
-    return _time;
+    return _time; //returns the time of the sample (IE: how long the note is to be played)
 }
 sf::Int16 StringSound::sample() {
-    return _cb->peek();
+    return _cb->peek(); //returns the peek of the sample
 }
-StringSound::~StringSound() {}
+StringSound::~StringSound() {} //deconstructor
